e0c08096d9e27203e7597a3269f4476abef44534
Igor Heleno <marioanimes09@gmail.com>
2023-10-08 16:58:59 -0300

    sexto

---
+++ OneDrive/Área de Trabalho/newWord/src/components/Section/Section.jsx
@@ -0,0 +1,64 @@
+import React from  'react';
+import img1 from '../../img/Clima.jpg';
+import img2 from '../../img/sociedade 1.jpg';
+import img3 from '../../img/sociedade 2.jpg';
+import img4 from '../../img/sociedade.jpg';
+import img5 from '../../img/chegando.jpg';
+import img6 from '../../img/primeira.jpg';
+import './Section.scss';
+
+function Section (){
+    return(
+        <>
+            <section className="conteiner">
+                <div>
+                <img src={img1} alt="" className="img-sociedade" />
+                <span className="back-img"></span>
+                <p className="text-img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis <span className="color">dfdweudfewd</span> voluptatibus, magni cumque ratione consequuntur enim similique nostrum, sit maxime aliquid numquam perferendis excepturi, dolor ipsam laboriosam unde ab quam nulla?</p>
+                </div>
+                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit quis excepturi fuga obcaecati hic, corporis totam eaque vero repellat cumque reiciendis reprehenderit ex tempore distinctio ut nobis temporibus dolorum. Repudiandae debitis quidem temporibus consequatur quae quaerat dolorum excepturi, laboriosam odit, voluptas adipisci libero assumenda optio aspernatur est, distinctio eum exercitationem accusamus labore explicabo veniam corporis? Optio alias eius enim! Inventore nesciunt nemo ab libero vitae, corrupti et error neque, recusandae excepturi alias cupiditate mollitia fuga corporis minima expedita animi. Pariatur commodi suscipit consectetur, soluta tenetur inventore expedita. Unde, laboriosam. Itaque veniam sapiente doloribus cumque ea dolor accusamus error et.</p>
+            </section>
+            <section className="conteiner">
+                <div>
+                <img src={img2} alt="" className="img-sociedade" />
+                <span className="back-img"></span>
+                <p className="text-img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis <span className="color">dfdweudfewd</span> voluptatibus, magni cumque ratione consequuntur enim similique nostrum, sit maxime aliquid numquam perferendis excepturi, dolor ipsam laboriosam unde ab quam nulla?</p>
+                </div>
+                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit quis excepturi fuga obcaecati hic, corporis totam eaque vero repellat cumque reiciendis reprehenderit ex tempore distinctio ut nobis temporibus dolorum. Repudiandae debitis quidem temporibus consequatur quae quaerat dolorum excepturi, laboriosam odit, voluptas adipisci libero assumenda optio aspernatur est, distinctio eum exercitationem accusamus labore explicabo veniam corporis? Optio alias eius enim! Inventore nesciunt nemo ab libero vitae, corrupti et error neque, recusandae excepturi alias cupiditate mollitia fuga corporis minima expedita animi. Pariatur commodi suscipit consectetur, soluta tenetur inventore expedita. Unde, laboriosam. Itaque veniam sapiente doloribus cumque ea dolor accusamus error et.</p>
+            </section>
+            <section className="conteiner">
+                <div>
+                <img src={img3} alt="" className="img-sociedade" />
+                <span className="back-img"></span>
+                <p className="text-img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis <span className="color">dfdweudfewd</span> voluptatibus, magni cumque ratione consequuntur enim similique nostrum, sit maxime aliquid numquam perferendis excepturi, dolor ipsam laboriosam unde ab quam nulla?</p>
+                </div>
+                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit quis excepturi fuga obcaecati hic, corporis totam eaque vero repellat cumque reiciendis reprehenderit ex tempore distinctio ut nobis temporibus dolorum. Repudiandae debitis quidem temporibus consequatur quae quaerat dolorum excepturi, laboriosam odit, voluptas adipisci libero assumenda optio aspernatur est, distinctio eum exercitationem accusamus labore explicabo veniam corporis? Optio alias eius enim! Inventore nesciunt nemo ab libero vitae, corrupti et error neque, recusandae excepturi alias cupiditate mollitia fuga corporis minima expedita animi. Pariatur commodi suscipit consectetur, soluta tenetur inventore expedita. Unde, laboriosam. Itaque veniam sapiente doloribus cumque ea dolor accusamus error et.</p>
+            </section>
+            <section className="conteiner">
+                <div>
+                <img src={img4} alt="" className="img-sociedade" />
+                <span className="back-img"></span>
+                <p className="text-img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis <span className="color">dfdweudfewd</span> voluptatibus, magni cumque ratione consequuntur enim similique nostrum, sit maxime aliquid numquam perferendis excepturi, dolor ipsam laboriosam unde ab quam nulla?</p>
+                </div>
+                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit quis excepturi fuga obcaecati hic, corporis totam eaque vero repellat cumque reiciendis reprehenderit ex tempore distinctio ut nobis temporibus dolorum. Repudiandae debitis quidem temporibus consequatur quae quaerat dolorum excepturi, laboriosam odit, voluptas adipisci libero assumenda optio aspernatur est, distinctio eum exercitationem accusamus labore explicabo veniam corporis? Optio alias eius enim! Inventore nesciunt nemo ab libero vitae, corrupti et error neque, recusandae excepturi alias cupiditate mollitia fuga corporis minima expedita animi. Pariatur commodi suscipit consectetur, soluta tenetur inventore expedita. Unde, laboriosam. Itaque veniam sapiente doloribus cumque ea dolor accusamus error et.</p>
+            </section>
+            <section className="conteiner">
+                <div>
+                <img src={img5} alt="" className="img-sociedade" />
+                <span className="back-img"></span>
+                <p className="text-img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis <span className="color">dfdweudfewd</span> voluptatibus, magni cumque ratione consequuntur enim similique nostrum, sit maxime aliquid numquam perferendis excepturi, dolor ipsam laboriosam unde ab quam nulla?</p>
+                </div>
+                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit quis excepturi fuga obcaecati hic, corporis totam eaque vero repellat cumque reiciendis reprehenderit ex tempore distinctio ut nobis temporibus dolorum. Repudiandae debitis quidem temporibus consequatur quae quaerat dolorum excepturi, laboriosam odit, voluptas adipisci libero assumenda optio aspernatur est, distinctio eum exercitationem accusamus labore explicabo veniam corporis? Optio alias eius enim! Inventore nesciunt nemo ab libero vitae, corrupti et error neque, recusandae excepturi alias cupiditate mollitia fuga corporis minima expedita animi. Pariatur commodi suscipit consectetur, soluta tenetur inventore expedita. Unde, laboriosam. Itaque veniam sapiente doloribus cumque ea dolor accusamus error et.</p>
+            </section>
+            <section className="conteiner">
+                <div>
+                <img src={img6} alt="" className="img-sociedade" />
+                <span className="back-img"></span>
+                <p className="text-img">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis <span className="color">dfdweudfewd</span> voluptatibus, magni cumque ratione consequuntur enim similique nostrum, sit maxime aliquid numquam perferendis excepturi, dolor ipsam laboriosam unde ab quam nulla?</p>
+                </div>
+                <p className="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni impedit quis excepturi fuga obcaecati hic, corporis totam eaque vero repellat cumque reiciendis reprehenderit ex tempore distinctio ut nobis temporibus dolorum. Repudiandae debitis quidem temporibus consequatur quae quaerat dolorum excepturi, laboriosam odit, voluptas adipisci libero assumenda optio aspernatur est, distinctio eum exercitationem accusamus labore explicabo veniam corporis? Optio alias eius enim! Inventore nesciunt nemo ab libero vitae, corrupti et error neque, recusandae excepturi alias cupiditate mollitia fuga corporis minima expedita animi. Pariatur commodi suscipit consectetur, soluta tenetur inventore expedita. Unde, laboriosam. Itaque veniam sapiente doloribus cumque ea dolor accusamus error et.</p>
+            </section>
+        </>
+    );
+}
+export default Section;
--- OneDrive/Área de Trabalho/newWord/src/pages/Home/Home.jsx
+++ OneDrive/Área de Trabalho/newWord/src/pages/Home/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Card from '../../components/Card/Card';
-import './Home.scss';
 import Hero from '../../components/Hero/Hero';
+import './Home.scss';
+import Section from '../../components/Section/Section';
+
 
 
 function Home(){
-
-
-
   return(
-  
     <section className="home">
       <Hero/>
       <Card/>
+      <Section/>
     </section>
+    
     
   );
 }
